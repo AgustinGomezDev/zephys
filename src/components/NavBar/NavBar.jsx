c7264f0a9df1721dcaa36bed27ba0023c67dd287
AgustinGomezDev <2agustingomez3@gmail.com>
2022-12-16 14:28:37 -0300

    Add hover animations

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,15 +5,15 @@ export const NavBar = () => {
         <div className="w-screen flex justify-between items-center p-4 bg-darkBgColor">
             <div>
                 <a href="#">
-                    <img className="w-14" src="./src/assets/zephys_color_min.png" alt="Zephys logo" />
+                    <img className="w-14 hover:brightness-75 ease-in-out duration-200" src="./src/assets/zephys_color_min.png" alt="Zephys logo" />
                 </a>
             </div>
             <div>
                 <ul className="flex gap-8 text-xl uppercase text-white flex-col lg:flex-row">
-                    <li> <a href="#" className="text-current font-bold hover:text-primaryColor">Home</a> </li>
-                    <li> <a href="#" className="text-current font-bold hover:text-primaryColor">Products</a> </li>
-                    <li> <a href="#" className="text-current font-bold hover:text-primaryColor">About us</a> </li>
-                    <li> <a href="#" className="text-current font-bold hover:text-primaryColor">Contact</a> </li>
+                    <li> <a href="#" className="text-current font-bold hover:text-primaryColor ease-in-out duration-200">Home</a> </li>
+                    <li> <a href="#" className="text-current font-bold hover:text-primaryColor ease-in-out duration-200">Products</a> </li>
+                    <li> <a href="#" className="text-current font-bold hover:text-primaryColor ease-in-out duration-200">About us</a> </li>
+                    <li> <a href="#" className="text-current font-bold hover:text-primaryColor ease-in-out duration-200">Contact</a> </li>
                 </ul>
             </div>
             <CartWidget />
