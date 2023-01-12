4f3044ec4e4ebe0042d283a45f55b18abd9bd5a1
AgustinGomezDev <2agustingomez3@gmail.com>
2023-01-12 10:32:32 -0300

    Add Contact events to inputs

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,6 +1,24 @@
 import { HiGlobeAmericas, HiMapPin, HiPhone, HiFingerPrint } from "react-icons/hi2";
+import { useState } from "react"
 
 export const Contact = () => {
+    const [values, setValues] = useState({
+        name: '',
+        email: '',
+        message: ''
+    })
+
+    const handleInputChange = (e) => {
+        setValues({
+            ...values, [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log('Submit:', values);
+    }
+
     return (
         <div className="relative">
             <img
@@ -28,14 +46,34 @@ export const Contact = () => {
                             <p className="text-xl font-semibold">Tax ID: 273 384</p>
                         </div>
                     </div>
-                    <form className="flex flex-col gap-5 justify-around mt-12">
-                        <input required type="text" className="text-lg border-2 border-slate-300 rounded p-3 focus:outline-none focus:border-primaryColor" placeholder="Name"/>
-                        <input required type="email" className="text-lg border-2 border-slate-300 rounded p-3 focus:outline-none focus:border-primaryColor" placeholder="Email"/>
-                        <textarea required className="text-lg border-2 border-slate-300 rounded p-3 resize-none focus:outline-none focus:border-2 focus:border-primaryColor" placeholder="Message"></textarea>
-                        <div className="flex items-center justify-center gap-5">
-                            <input type="checkbox" className="cursor-pointer w-5 h-5 accent-primaryColor" />
-                            <p>Send me a copy of this message</p>
-                        </div>
+                    <form className="flex flex-col gap-5 justify-around mt-12" onSubmit={handleSubmit}>
+                        <input
+                            required
+                            type="text"
+                            className="text-lg border-2 border-slate-300 rounded p-3 focus:outline-none focus:border-primaryColor"
+                            placeholder="Name"
+                            value={values.name}
+                            name='name'
+                            onChange={handleInputChange}
+                        />
+                        <input
+                            required
+                            type="email"
+                            className="text-lg border-2 border-slate-300 rounded p-3 focus:outline-none focus:border-primaryColor"
+                            placeholder="Email"
+                            value={values.email}
+                            name='email'
+                            onChange={handleInputChange}
+                        />
+                        <textarea
+                            required
+                            className="text-lg border-2 border-slate-300 rounded p-3 resize-none focus:outline-none focus:border-2 focus:border-primaryColor"
+                            placeholder="Message"
+                            value={values.message}
+                            name='message'
+                            onChange={handleInputChange}>
+                        </textarea>
+                        <p className="text-center">Our representative will get back to you within 24 hours.</p>
                         <input type="submit" value="Send" className="text-lg border-2 bg-primaryColor text-white border-slate-300 rounded p-3 focus:outline-none active:border-primaryColor active:brightness-75 cursor-pointer" />
                     </form>
                 </div>
