e843f18e87c8c41d48d56d86b435ec1f6ec4becb
AgustinGomezDev <2agustingomez3@gmail.com>
2023-01-17 10:21:18 -0300

    Add message if the cart is empty

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,6 +1,6 @@
 import { useContext } from "react"
 import { CartContext } from "../../context/CartContext"
-import { useNavigate } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 
 export const Cart = () => {
     const { cart, emptyCart, totalPrice, removeFromCart } = useContext(CartContext)
@@ -9,6 +9,19 @@ export const Cart = () => {
 
     const handleBack = () => {
         navigate(-1)
+    }
+
+    if(cart.length === 0 ){
+        return (
+            <div className="container mx-auto min-h-screen">
+                <h2 className="inline-block text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-tr from-pink-300 to-purple-900 leading-tight">Your cart is empty</h2>
+                <hr />
+                <div className="mt-5">
+                    <p className="text-xl font-medium">Let's buy something cool for your pet.</p>
+                    <Link to="/" className="mt-3 rounded-lg text-center px-5 py-2.5 border-2 border-transparent block w-60 text-white bg-darkBgColor hover:border-primaryColor focus:outline-none">Go shopping</Link>
+                </div>
+            </div>
+        )
     }
 
     return(
@@ -20,14 +33,14 @@ export const Cart = () => {
                     <div className="px-5 py-1 bg-white even:bg-gray-100 mt-3 w-full flex items-center justify-between" key={item.id}>
                         <div>
                             <h4 className="text-lg font-bold">{item.name}</h4>
-                            <p className="text-lg font-medium">Amount: {item.amount}</p>
-                            <p className="text-lg font-medium">Unit price: ${item.price}</p>
-                            <p className="text-lg font-medium">Total price: ${(item.price * item.amount).toFixed(2)}</p>
+                               <p className="text-lg font-medium">Amount: {item.amount}</p>
+                                <p className="text-lg font-medium">Unit price: ${item.price}</p>
+                                <p className="text-lg font-medium">Total price: ${(item.price * item.amount).toFixed(2)}</p>
                         </div>
                         <div className="flex items-center gap-5">
                             <button onClick={() => removeFromCart(item.id)} className="block font-bold text-white bg-red-500 hover:border-red-200 active:bg-red-800 focus:outline-none">Remove from cart</button>
                         </div>
-                    </div>
+                     </div>
                 ))
             }
             <hr />
